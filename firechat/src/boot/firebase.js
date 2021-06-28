a0817005df37d03536adbf23779711533a87dcab
juanperdomolol <juanpb902@gmail.com>
2021-06-28 16:24:34 -0500

    configuraciones firebase, quasar

---
+++ firechat/src/boot/firebase.js
@@ -0,0 +1,26 @@
+import { boot } from 'quasar/wrappers'
+
+import 'firebase/auth'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB7i3KjXoY6ublWN7UIroTa8Bt3FpFEXZk",
+  authDomain: "fire-chat-quasar.firebaseapp.com",
+  projectId: "fire-chat-quasar",
+  storageBucket: "fire-chat-quasar.appspot.com",
+  messagingSenderId: "866927374653",
+  appId: "1:866927374653:web:b9dfa2c3553b5d64ae9554"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+const marcaTiempo = firebase.firestore.FieldValue.serverTimestamp;
+
+export { db,auth, marcaTiempo}
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async (/* { app, router, ... } */) => {
+  // something to do
+})
